c31da0aad43e1106f1f96863bfbea9d845b509f0
meganedogYuu <tkra.yuuki@gmail.com>
2018-04-04 22:41:34 +0900

    :sparkles: add rgbColorToHsv

--- src/utility/convert.ts
+++ src/utility/convert.ts
@@ -77,6 +77,45 @@ export function rgbColorToHex(rgbColor: RgbColor): string {
 }
 
 /**
+ * RgbColorオブジェクトをhsv値に変換する
+ * それぞれのとれる値の範囲は以下とし、また値を丸めずに返す
+ * H（色相）： 0 ~ 360
+ * S（彩度）： 0 ~ 100
+ * V（明度）： 0 ~ 100
+ *
+ * @param {RgbColor} rgbColor
+ * @returns { h: number; s: number; v: number }
+ */
+export function rgbColorToHsv(rgbColor: RgbColor): { h: number, s: number, v: number } {
+  const max = Math.max(rgbColor.r, rgbColor.g, rgbColor.b);
+  const min = Math.min(rgbColor.r, rgbColor.g, rgbColor.b);
+
+  // minとmaxが同じ無彩色の場合は、色相と彩度は存在しないため、明度のみ設定して返す
+  if (max === min) return { h: 0, s: 0, v: (max / 255) * 100 };
+
+  // 色相の値を求める
+  let h = 0;
+  if (rgbColor.r === max) {
+    h = 60 * ((rgbColor.g - rgbColor.b) / (max - min));
+  }else if (rgbColor.g === max) {
+    h = 60 * ((rgbColor.b - rgbColor.r) / (max - min)) + 120;
+  }else if (rgbColor.b === max) {
+    h = 60 * ((rgbColor.r - rgbColor.g) / (max - min)) + 240;
+  }
+  // hがマイナス値になった場合は360を足す事で0 ~ 360 の範囲に収める
+  if (h < 0) h += 360;
+
+  // 彩度
+  const s = ((max - min) / max) * 100;
+
+  // 明度
+  const v = (max / 255) * 100;
+
+  return { h, s, v };
+}
+
+
+/**
  * 受け取ったRgbColorの反対色を求め、RgbColorとして返す
  *
  * @param {RgbColor} rgbColor
