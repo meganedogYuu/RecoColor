37b2b6a97d43d070d33e479060ad1854117a6364
meganedogYuu <tkra.yuuki@gmail.com>
2018-05-01 22:17:00 +0900

    tests: Add getDyad test

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -390,6 +390,107 @@ describe('RecoColor test', () => {
 
 
   /**
+   * getDyad
+   */
+  describe('getDyad test', () => {
+    describe('正常値の場合の getDyad の値 を取得する（オプション指定なし）', () => {
+      const tests = ['#f00', 'rgb(200, 10, 1)', {h: 234, s: 50, v:96}];
+      const answers = [[{r: 0, g: 255, b: 255}], [{r: 1, g: 191, b: 200}], [{r: 245, g: 233, b: 122}]];
+
+      tests.forEach((element, index) => {
+        const color = new recoColor(tests[index]);
+        const dyad = color.getDyad();
+        const answer = answers[index];
+        dyad.forEach((c, i) => {
+          it(`${JSON.stringify(element)} getDyad element: ${i} is ${JSON.stringify(answer[i])}`, () => {
+            assert.strictEqual(c.r, answer[i].r);
+            assert.strictEqual(c.g, answer[i].g);
+            assert.strictEqual(c.b, answer[i].b);
+          });
+        });
+      });
+    });
+
+    describe('正常値の場合の getDyad の値 を取得する（hasOriginal: true）', () => {
+      const tests = ['#f00', 'rgb(200, 10, 1)', {h: 234, s: 50, v:96}];
+      const answers = [[{r: 255, g: 0, b: 0}, {r: 0, g: 255, b: 255}], [{ r: 200, g: 10, b: 1 }, {r: 1, g: 191, b: 200}], [{ r: 122, g: 135, b: 245 }, {r: 245, g: 233, b: 122}]];
+
+      tests.forEach((element, index) => {
+        const color = new recoColor(tests[index]);
+        const dyad = color.getDyad({hasOriginal: true});
+        const answer = answers[index];
+        dyad.forEach((c, i) => {
+          it(`${JSON.stringify(element)} getDyad element: ${i} is ${JSON.stringify(answer[i])}`, () => {
+            assert.strictEqual(c.r, answer[i].r);
+            assert.strictEqual(c.g, answer[i].g);
+            assert.strictEqual(c.b, answer[i].b);
+          });
+        });
+      });
+    });
+
+    describe('正常値の場合の getDyad の値 を取得する（type: hex）', () => {
+      const tests = ['#f00', 'rgb(200, 10, 1)', {h: 234, s: 50, v:96}];
+      const answers = [['00ffff'], ['01bfc8'], ['f5e97a']];
+
+      tests.forEach((element, index) => {
+        const color = new recoColor(tests[index]);
+        const dyad = color.getDyad({type: 'hex'});
+        const answer = answers[index];
+        dyad.forEach((c, i) => {
+          it(`${JSON.stringify(element)} getDyad element: ${i} is ${JSON.stringify(answer[i])}`, () => {
+            assert.strictEqual(c, answer[i]);
+          });
+        });
+      });
+    });
+
+    describe('正常値の場合の getDyad の値 を取得する（type: hsv）', () => {
+      const tests = ['#f00', 'rgb(200, 10, 1)', {h: 234, s: 50, v:96}];
+      const answers = [[{h: 180, s: 100, v: 100}], [{h: 183, s: 100, v: 78}], [{h: 54, s: 50, v: 96}]];
+
+      tests.forEach((element, index) => {
+        const color = new recoColor(tests[index]);
+        const dyad = color.getDyad({type: 'hsv'});
+        const answer = answers[index];
+        dyad.forEach((c, i) => {
+          it(`${JSON.stringify(element)} getDyad element: ${i} is ${JSON.stringify(answer[i])}`, () => {
+            assert.strictEqual(c.h, answer[i].h);
+            assert.strictEqual(c.s, answer[i].s);
+            assert.strictEqual(c.v, answer[i].v);
+          });
+        });
+      });
+    });
+
+    describe('正常値の場合の getDyad の値 を取得する（hasOriginal, type両方指定）', () => {
+      const tests = ['#f00', 'rgb(200, 10, 1)', {h: 234, s: 50, v:96}];
+      const answers = [['ff0000', '00ffff'], ['c80a01', '01bfc8'], ['7a87f5', 'f5e97a']];
+
+      tests.forEach((element, index) => {
+        const color = new recoColor(tests[index]);
+        const dyad = color.getDyad({hasOriginal:true, type: 'hex'});
+        const answer = answers[index];
+        dyad.forEach((c, i) => {
+          it(`${JSON.stringify(element)} getDyad element: ${i} is ${JSON.stringify(answer[i])}`, () => {
+            assert.strictEqual(c, answer[i]);
+          });
+        });
+      });
+    });
+
+    describe('異常値の場合は undefined を取得する', () => {
+      const tests = ['#ffff', '#fffffff', '$fff', '#ffg', [0, 10, 255, 255], 'rgb(0, 120, 255, 0)', 'RGBA(180, 20, 90)', {r: 256, g: 10, b: 180}];
+
+      tests.forEach((element, index) => {
+        let color = new recoColor(tests[index]);
+        it(`${element} getDyad is undefined`, () => { assert.strictEqual(color.getDyad(), undefined) });
+      });
+    });
+  });
+
+
+  /**
    * getTriad
    */
   describe('getTriad test', () => {
