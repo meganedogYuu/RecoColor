0808eb22558113fb94e0f41f6a90fb203a7c444f
meganedogYuu <tkra.yuuki@gmail.com>
2018-05-01 22:42:26 +0900

    tests: Add getSplitComplementary test

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -932,4 +932,125 @@ describe('RecoColor test', () => {
       });
     });
   });
+
+
+  /**
+   * getSplitComplementary
+   */
+  describe('getSplitComplementary test', () => {
+    describe('正常値の場合の getSplitComplementary の値 を取得する（オプション指定なし）', () => {
+      const tests = ['#f00', 'rgb(200, 10, 1)', {h: 234, s: 50, v:96}];
+      const answers = [
+        [{r: 0, g: 255, b: 128}, {r: 0, g: 128, b: 255}],
+        [{r: 1, g: 200, b: 109}, {r: 1, g: 92, b: 200}],
+        [{r: 245, g: 171, b: 122}, {r: 196, g: 245, b: 122}]
+      ];
+
+      tests.forEach((element, index) => {
+        const color = new recoColor(tests[index]);
+        const sc = color.getSplitComplementary();
+        const answer = answers[index];
+        sc.forEach((c, i) => {
+          it(`${JSON.stringify(element)} getSplitComplementary element: ${i} is ${JSON.stringify(answer[i])}`, () => {
+            assert.strictEqual(c.r, answer[i].r);
+            assert.strictEqual(c.g, answer[i].g);
+            assert.strictEqual(c.b, answer[i].b);
+          });
+        });
+      });
+    });
+
+    describe('正常値の場合の getSplitComplementary の値 を取得する（hasOriginal: true）', () => {
+      const tests = ['#f00', 'rgb(200, 10, 1)', {h: 234, s: 50, v:96}];
+      const answers = [
+        [{r: 255, g: 0, b: 0}, {r: 0, g: 255, b: 128}, {r: 0, g: 128, b: 255}],
+        [{r: 200, g: 10, b: 1}, {r: 1, g: 200, b: 109}, {r: 1, g: 92, b: 200}],
+        [{r: 122, g: 135, b: 245}, {r: 245, g: 171, b: 122}, {r: 196, g: 245, b: 122}]
+      ];
+
+      tests.forEach((element, index) => {
+        const color = new recoColor(tests[index]);
+        const sc = color.getSplitComplementary({hasOriginal: true});
+        const answer = answers[index];
+        sc.forEach((c, i) => {
+          it(`${JSON.stringify(element)} getSplitComplementary element: ${i} is ${JSON.stringify(answer[i])}`, () => {
+            assert.strictEqual(c.r, answer[i].r);
+            assert.strictEqual(c.g, answer[i].g);
+            assert.strictEqual(c.b, answer[i].b);
+          });
+        });
+      });
+    });
+
+    describe('正常値の場合の getSplitComplementary の値 を取得する（type: hex）', () => {
+      const tests = ['#f00', 'rgb(200, 10, 1)', {h: 234, s: 50, v:96}];
+      const answers = [
+        ['00ff80', '0080ff'],
+        ['01c86d', '015cc8'],
+        ['f5ab7a', 'c4f57a']
+      ];
+
+      tests.forEach((element, index) => {
+        const color = new recoColor(tests[index]);
+        const sc = color.getSplitComplementary({type: 'hex'});
+        const answer = answers[index];
+        sc.forEach((c, i) => {
+          it(`${JSON.stringify(element)} getSplitComplementary element: ${i} is ${JSON.stringify(answer[i])}`, () => {
+            assert.strictEqual(c, answer[i]);
+          });
+        });
+      });
+    });
+
+    describe('正常値の場合の getSplitComplementary の値 を取得する（type: hsv）', () => {
+      const tests = ['#f00', 'rgb(200, 10, 1)', {h: 234, s: 50, v:96}];
+      const answers = [
+        [{h: 150, s: 100, v: 100}, {h: 210, s: 100, v: 100}],
+        [{h: 153, s: 100, v: 78}, {h: 213, s: 100, v: 78}],
+        [{h: 24, s: 50, v: 96}, {h: 84, s: 50, v: 96}]
+      ];
+
+      tests.forEach((element, index) => {
+        const color = new recoColor(tests[index]);
+        const sc = color.getSplitComplementary({type: 'hsv'});
+        const answer = answers[index];
+        sc.forEach((c, i) => {
+          it(`${JSON.stringify(element)} getSplitComplementary element: ${i} is ${JSON.stringify(answer[i])}`, () => {
+            assert.strictEqual(c.h, answer[i].h);
+            assert.strictEqual(c.s, answer[i].s);
+            assert.strictEqual(c.v, answer[i].v);
+          });
+        });
+      });
+    });
+
+    describe('正常値の場合の getSplitComplementary の値 を取得する（hasOriginal, type両方指定）', () => {
+      const tests = ['#f00', 'rgb(200, 10, 1)', {h: 234, s: 50, v:96}];
+      const answers = [
+        ['ff0000', '00ff80', '0080ff'],
+        ['c80a01', '01c86d', '015cc8'],
+        ['7a87f5', 'f5ab7a', 'c4f57a']
+      ];
+
+      tests.forEach((element, index) => {
+        const color = new recoColor(tests[index]);
+        const sc = color.getSplitComplementary({hasOriginal:true, type: 'hex'});
+        const answer = answers[index];
+        sc.forEach((c, i) => {
+          it(`${JSON.stringify(element)} getSplitComplementary element: ${i} is ${JSON.stringify(answer[i])}`, () => {
+            assert.strictEqual(c, answer[i]);
+          });
+        });
+      });
+    });
+
+    describe('異常値の場合は undefined を取得する', () => {
+      const tests = ['#ffff', '#fffffff', '$fff', '#ffg', [0, 10, 255, 255], 'rgb(0, 120, 255, 0)', 'RGBA(180, 20, 90)', {r: 256, g: 10, b: 180}];
+
+      tests.forEach((element, index) => {
+        let color = new recoColor(tests[index]);
+        it(`${element} getSplitComplementary is undefined`, () => { assert.strictEqual(color.getSplitComplementary(), undefined) });
+      });
+    });
+  });
 });
