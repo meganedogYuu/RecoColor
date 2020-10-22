f9389d5f6aa1b76811fbac0424a529db59b60c9b
yuki-takara <tkra.yuuki@gmail.com>
2020-10-23 04:34:29 +0900

    test: util.spec.jsをjest化

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     // TODO: 1テストファイルを修正するのが重いので、1つずつ直していく
     '**/__tests__/check.spec.js',
     '**/__tests__/convert.spec.js',
+    '**/__tests__/util.spec.js',
   ],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
